f85e0c08c993ca9c60560c41716a2623a5a02398
David Faitelson <david.faitelson@gmail.com>
2012-08-15 13:48:09 +0300

    wpca now creates a more natural analysis model.

--- Alloy/Analyzer.hs
+++ Alloy/Analyzer.hs
@@ -76,7 +76,7 @@ generate cfg theory records code =
      [analysisFile] <- lookupM "alloy.analysisfile" cfg
      libraries <- lookupM "alloy.analysislibraries" cfg
      putStrLn ("... There are " ++ (show (length obligs)) ++ " proof obligations. Writing analysis file to " ++ analysisFile)
-     writeFile analysisFile (showModel (libraries++[s | Node (_,String s) [] <- theory]) records stateVars constants obligs)
+     writeFile analysisFile (showModelX (libraries++[s | Node (_,String s) [] <- theory]) records stateVars constants obligs)
      return (obligs, stateVars ++ constants)
 
 -- Alloy does not care if the join is due to an array or due to a relation
@@ -190,6 +190,11 @@ showModel libraries records stateVars constants obligs =
  ++ (showConstraints (stateVars ++ constants)) ++ "\n\n"
  ++ (foldr (++) "" (map showOblig obligs))
 
+showModelX libraries records stateVars constants obligs =
+ "module wpca_analysis [" ++ (showRecordsX records) ++ "]\n"
+ ++ (foldr (++) "" [ "open " ++ s ++ "\n" | s <- libraries])
+ ++ (foldr (++) "" (map (showObligX (stateVars ++ constants)) obligs))
+
 showRecords :: [AST] -> String
 showRecords = foldr (++) [] . map showRecord
 
@@ -202,14 +207,31 @@ showRecord (Node (_,Record name) defs) = header ++ fields ++ footer
 showDef :: AST -> String
 showDef x = ""
 
+showRecordsX :: [AST] -> String
+showRecordsX = separateByComma . map f
+  where f (Node (_,Record name) _) = name
+
 showConstraints :: Env -> String
 showConstraints env = foldr (++) "" (map f env)
  where
  f (n, Node (_, Type "nat") []) = "fact { all s : State | s."++n++".gte[0] }\n"
  f (n, Node (_, ArrayType k _) []) = "fact { all s : State | #s."++n++"=s."++k++"}\n"
  f _ = ""
+
+showConstraintsX :: Env -> String
+showConstraintsX env = "{" ++ (foldr (++) "" (map f env)) ++ "}"
+ where
+ f (n, Node (_, Type "nat") []) = "(" ++ n++".gte[0])\n"
+ f (n, Node (_, ArrayType k _) []) = "(#"++n++"="++k++")\n"
+ f _ = ""
  
 showOblig (nm, (wp, path, goal)) = "\n/*\ngoal: " ++ goal ++ "\npath: " ++ (show path) ++ "\n*/\n" ++ "assert " ++ nm ++ " {\n" ++ (showA wp) ++ "\n}\ncheck " ++ nm ++ "\n\n"
+
+showObligX env (nm, (wp, path, goal)) = comment ++ pred ++ assertion ++ check
+  where comment = "\n/*\ngoal: " ++ goal ++ "\npath: " ++ (show path) ++ "\n*/\n" 
+        pred = "pred pred_" ++ nm ++ "[" ++ (showEnv env) ++ "]\n{\n"  ++ (showConstraintsX env) ++ " =>\n\t" ++ (showAX wp) ++ "\n}\n"
+        assertion = "assert " ++ nm ++ " {\nall " ++ (showEnv env) ++ "| pred_" ++ nm ++ "[" ++ (showEnvNames env) ++ "]" ++ "\n}\n"
+        check = "check " ++ nm ++ "\n\n"
 
 showEnv :: Env -> String
 showEnv [] = ""
@@ -218,6 +240,9 @@ showEnv ((s,n):ns) = showBinding (s,n) ++ ",\n" ++ (showEnv ns)
 
 showBinding :: (String,AST) -> String
 showBinding (name, x) = name ++ " : " ++ (showType x)
+
+showEnvNames :: Env -> String
+showEnvNames = showNames . fst . unzip 
 
 showType :: AST -> String
 showType = foldRose f
@@ -281,6 +306,57 @@ showA = foldRose f
         f (_, Update) [x,y] = "(" ++ x ++ " ++ " ++ y  ++ ")"
 	f (_, Closure) [x] = x
 	f other ns = error ("Internal error: Don't know how to show " ++ (show other))
+
+showAX :: AST -> String
+showAX = foldRose f
+  where f (_, Plus) [x,y] = x ++ ".add[" ++ y ++ "]"
+        f (_, Minus) [x,y] = x ++ ".sub[" ++ y ++ "]"
+        f (_, Times) [x,y] = x ++ ".mul[" ++ y ++"]"
+        f (_, Div) [x,y] = x ++ ".div[" ++ y ++"]"
+        f (_, Mod) [x,y] = x ++ ".rem[" ++ y ++"]"
+        f (_, Quotient) [x,y] = "(" ++ x ++ " / " ++ y ++ ")"
+        f (_, Eq) [x,y] = x ++ " = " ++ y
+        f (_, NotEq) [x,y] = x ++ " != " ++ y
+        f (_, Greater) [x,y] = x ++ " > " ++ y
+        f (_, Less) [x,y] = x ++ " < " ++ y
+        f (_, Geq) [x,y] = x ++ " >= " ++ y
+        f (_, Leq) [x,y] = x ++ " <= " ++ y
+        f (_, In) [x,y] = "(" ++ x ++ " in " ++ y ++ ")"
+        f (_, Conj) [x,y] = x ++ " and " ++ y
+        f (_, Disj) [x,y] = "("++ x ++ " or " ++ y++")"
+        f (_, Implies) [x,y] = "(" ++ x ++ " => " ++ y ++")"
+        f (_, Range) [x,y] = "range[" ++ x ++ "," ++ y ++ "]"
+        f (_, Join) xs = (showJoin xs)
+        f (_, Product) xs = (showRel xs)
+        f (_, Not) [x] = "!(" ++ x ++ ")"
+        f (_, Neg) [x] = x ++ ".negate"
+        f (_, Reverse) [x] = "~(" ++ x ++ ")"
+        f (_, Int x) [] = show x
+        f (_, AST.True) [] = "true"
+        f (_, AST.False) [] = "false"
+        f (_, Const) [x] = x
+        f (_, Type "int") [] = "Int"
+        f (_, Type "nat") [] = "Int"
+        f (_, Type n) [] = n
+	f (_, Output) [x] = x
+        f (_, ArrayType _ t) [] = if t == "int" then "seq Int" else ("seq " ++ t)
+        f (_, Declaration) [names, typ] = names ++ " : " ++ typ
+        f (_, List) names = showNames names
+        f (_, Quantifier Sum) [decls, e] = "(sum " ++ decls ++ " | " ++ e ++ ")"
+        f (_, Quantifier All) [decls, e] = "(all " ++ decls ++ " | " ++ e ++ ")"
+        f (_, Quantifier No) [decls, e] = "(no " ++ decls ++ " | " ++ e ++ ")"
+        f (_, Quantifier Some) [decls, e] = "(some " ++ decls ++ " | " ++ e ++ ")"
+        f (_, SomeSet) [e] = "(some " ++ e ++ ")"
+        f (_, String n) [] = n
+        f (_, StateVar n) [] = n
+        f (_, ConstVar n) [] = n
+        f (_, Pair) [x,y] = "(" ++ x ++ " -> " ++ y  ++ ")"
+        f (_, Union) [x,y] = "(" ++ x ++ " + " ++ y  ++ ")"
+        f (_, SetDiff) [x,y] = "(" ++ x ++ " - " ++ y  ++ ")"
+        f (_, Update) [x,y] = "(" ++ x ++ " ++ " ++ y  ++ ")"
+	f (_, Closure) [x] = x
+	f other ns = error ("Internal error: Don't know how to show " ++ (show other))
+
 
 showRel = foldr f ""
   where f x [] = x
--- list2.als
+++ list2.als
@@ -3,8 +3,6 @@ module list
 open partialOrder[univ] 
 open misc
 
-sig Node extends Object {}
-
 pred nullterminated[r : univ -> univ, h : univ] {
 	list[r]
 	NULL in h.*r
