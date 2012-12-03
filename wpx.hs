23e675bfc8f5721fee1a4b7c46409606b162ef2e
David Faitelson <david@proactivemodeling.com>
2012-12-03 09:07:04 +0200

    added wp rule for specification statement

--- wpx.hs
+++ wpx.hs
@@ -1,12 +1,10 @@
 
-
 module WPX where
 
 import Subst
 import Data.List
 
-
-data Stmt = Skip | Assign Def | Comp Stmt Stmt | Cond [Guard] 
+data Stmt = Skip | Assign Def | Comp Stmt Stmt | Cond [Guard] | Spec Pred Def Pred
 
 data Guard = Guard Pred Stmt
 
@@ -19,15 +17,18 @@ class WPX a where
   ptree :: a -> [Path] -> [Path]
 
 instance WPX Stmt where
-  wpx Skip pre = pre
-  wpx (Comp s1 s2) pre = wpx s1 (wpx s2 pre)
-  wpx (Assign def) pre = map (subst def) pre
-  wpx (Cond guards) pre = foldr (++) [] [ wpx g pre | g <- guards ]
+  wpx Skip post = post
+  wpx (Comp s1 s2) post = wpx s1 (wpx s2 post)
+  wpx (Assign def) post = map (subst def) post
+  wpx (Cond guards) post = foldr (++) [] [ wpx g post | g <- guards ]
+  wpx (Spec pre frame post) post' = 
+    [LogicOp Conj [pre, Quantifier All frame (LogicOp Impl [post, p])] | p <- post'] 
 
   ptree (Cond guards) path = foldr (++) [] [ ptree g path | g <- guards ]
   ptree (Comp s1 s2) path = ptree s1 (ptree s2 path)
   ptree _ path = path
 
 instance WPX Guard where
-  wpx (Guard g s) pre = [ LogicOp Impl [g, q] | q <- wpx s pre ]
+  wpx (Guard g s) post = [ LogicOp Impl [g, q] | q <- wpx s post ]
   ptree (Guard g s) path = [ g : p | p <- ptree s path ]
+
